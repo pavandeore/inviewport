e7c109143f6691f210e7f6f793ffe3caaedb08a7
pawan deore <pavandeore90@gmail.com>
2021-04-15 16:21:11 +0530

    files added

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+const box = document.querySelector('.box');
+const message = document.querySelector('#message');
+
+document.addEventListener('scroll', function () {
+    const messageText = isInViewport(box) ?
+        'The box is visible in the viewport' :
+        'The box is not visible in the viewport';
+
+    message.textContent = messageText;
+
+}, {
+    passive: true
+});
